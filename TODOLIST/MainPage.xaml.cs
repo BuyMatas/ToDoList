1da7a321ce25c917a4476950ce6a1868de9a7afb
Matas Norvaisas <matas.norvaisas@learnet.se>
2025-04-23 10:28:30 +0200

    first commit

---
+++ TODOLIST/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TODOLIST
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> TodoList { get; set; }
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            TodoList = new ObservableCollection<string>();
+
+            BindingContext = this;
+
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(AddListItem.Text))
+            {
+                TodoList.Add(AddListItem.Text); 
+            }
+        }
+    }
+
+}
